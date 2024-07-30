73ad11dabf5dd5eb9ec23fe54798b8916926473a
Victoria Munteanu <munteanuvictoria1@gmail.com>
2024-07-30 15:08:51 +0200

    update script

--- script.js
+++ script.js
@@ -33,9 +33,56 @@ toggle.addEventListener("click", (e) => {
   const html = document.querySelector("html");
   if (html.classList.contains("dark")) {
     html.classList.remove("dark");
-    e.target.innerHTML = "It/s dark!";
+    e.target.innerHTML = "Dark🐈‍⬛ ";
   } else {
     html.classList.add("dark");
-    e.target.innerHTML = "It/s bright!";
+    e.target.innerHTML = "Light🔥";
   }
 });
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hour = time.getHours();
+  const hoursForClock = hour % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hour >= 12 ? "PM" : "AM";
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeEL.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+  dateEL.innerHTML = `${days[day]}, ${months[month]} <span class="circle"> ${date}</span>`;
+
+  //Map a range of numbers to another range of numbers :
+  function scale(number, inMin, inMax, outMin, outMax) {
+    return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+  }
+}
+
+setTime();
+setInterval(setTime, 1000);
